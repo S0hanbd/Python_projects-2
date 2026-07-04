a83e929e83973d1394c2b7ddb78fd7a64eb0d166
S0hanbd <sohanbdtech@gmail.com>
2026-07-05 03:28:35 +0600

    third commit

---
+++ Derictories.py
@@ -0,0 +1,11 @@
+products = {
+    'Laptop': 990,
+    'Smartphone': 600,
+    'Tablet': 250,
+    'Headphones': 70,
+}
+
+print(products.keys())
+print(products['smartphone'])
+
+
---
+++ try.py
@@ -0,0 +1,6 @@
+products = {
+    'Laptop': 990,
+    'Smartphone': 600,
+    'Tablet': 250,
+    'Headphones': 70,
+}
